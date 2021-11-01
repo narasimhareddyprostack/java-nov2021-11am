9efa6cffc930eaf8bb934b54179decead8bc06ba
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-11-01 12:31:38 +0530

    first java program

---
+++ Basics/Product.java
@@ -0,0 +1,14 @@
+class Product{
+    public static void main(String[] args){
+       byte b = 127;
+       short s=30000;
+
+       int price = 499;
+       long l=543555552523532l;
+       float review = 4.3f;
+       double review1= 4.3;
+       char c = 's';
+       boolean discont = false;
+    }
+
+}
