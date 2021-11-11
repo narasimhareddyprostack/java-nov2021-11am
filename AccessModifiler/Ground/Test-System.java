8c62973c2594770f147afc106db9e866b0898c98
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-11-11 13:06:54 +0530

    okie

---
+++ AccessModifiler/Ground/Test-System.java
@@ -0,0 +1,6 @@
+import static java.lang.System.*;
+public class Test{
+    public static void main(String[] args){
+        out.println("Hello, Super....");
+    }
+}
---
+++ AccessModifiler/Ground/Test.java
@@ -0,0 +1,6 @@
+package com.airtel.user;
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello, Super....");
+    }
+}
