039795075093951721b3f0ccac61a59678210f77
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-12-13 12:36:30 +0530

    test

---
+++ Collection/src/CollectionDemo.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+public class CollectionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList al = new ArrayList();
+		
+		al.add("Rajini Kanth");
+		al.add(10);
+		al.add(10);al.add(10);
+		al.add(10);
+		System.out.println(al);
+		
+
+	}
+
+}
